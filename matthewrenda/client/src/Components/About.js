47245a05d3bbce60f30282c0b954c44db0877b82
MattRenda <matthewrenda14@yahoo.com>
2019-11-21 23:30:16 -0800

    commit

--- matthewrenda/client/src/Components/About.js
+++ matthewrenda/client/src/Components/About.js
@@ -9,26 +9,26 @@ class About extends Component {
             <>
 
                 <div className='about-width center '>
-                    <span className='about-intro'><b>A little bit about me.</b></span>
-                        <Row>
-                            <Col className='about-card'>
-                                <b>Education</b><br/><br/>
-                                I am currently attending California State University, Sacramento.
-                                My expected graduation date is May 2020 with a Bachelors in Computer Science.
+                    <span className='about-intro font'><b>A little bit about me.</b></span>
+                    <Row>
+                        <Col className='about-card font'>
+                            <b>Education</b><br /><br />
+                            I am currently attending California State University, Sacramento.
+                            My expected graduation date is May 2020 with a Bachelors in Computer Science.
                                 </Col>
-                            <Col className='about-card'>
-                                <b>Passion</b><br/><br/>
-                                I have always loved creating things. Seeing something through from start to 
-                                finish gives such a fullfilling feeling. This feeling brought me to web 
-                                development. As it is not only a wonderful way to create, but many people 
-                                can make use of your work. 
+                        <Col className='about-card font'>
+                            <b>Passion</b><br /><br />
+                            I have always loved creating things. Seeing something through from start to
+                            finish has always given me a sense of fullfillment. Because of this, I naturally
+                            gravitated to web development. As it is a wonderful way to create, and add value
+                            to many peoples lives.
+                                                            </Col>
+                        <Col className='about-card font'>
+                            <b>Drive</b><br /><br />
+                            I have grown quite a thirst for knowledge, and knowing that my work can always
+                            improve makes me strive to be better every day.
                                 </Col>
-                            <Col className='about-card'>
-                                <b>Drive</b><br/><br/>
-                                I have grown quite a thirst for knowledge and knowing that my work can always
-                                improve makes me strive to be better every day.
-                                </Col>
-                        </Row>
+                    </Row>
                 </div>
 
 
--- matthewrenda/client/src/Components/RecentWork.js
+++ matthewrenda/client/src/Components/RecentWork.js
@@ -6,7 +6,7 @@ class RecentWork extends Component {
 
     render() {
         return (
-            <div className='center work-smSpace'>
+            <div className='center work-smSpace' style={{marginBottom:'30vh'}}>
                 <Container justify-content-center>
                         <div >
                             <div className='work-introbig'>
@@ -19,14 +19,14 @@ class RecentWork extends Component {
                         </div>
 
                     <Row className='work-smSpace'>
-                        <Col className='background-image'>
+                        <Col className='background-image-ac' xs={6} md={4} >
                             <a href='https://atepex.herokuapp.com/' target='_blank'> <img src={require('../imgs/AC_heating_logo.png')}
                                 width='250'
                                 height='250'
                                 className='work-hover'
                                 href=''/></a>
                         </Col>
-                        <Col className='background-image'>
+                        <Col className='background-image-ib' xs={6} md={4}>
                             <a href='https://interviewbuddyapp.herokuapp.com' target='_blank'><img src={require('../imgs/Interviewbuddy_logo.png')}
                             width='250'
                             height='250'
