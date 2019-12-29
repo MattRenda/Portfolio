b0687c46b1d261a9f2001b50d9d240ff8f358f04
MattRenda <matthewrenda14@yahoo.com>
2019-12-28 16:39:05 -0800

    commit

--- matthewrenda/client/src/Components/Footer.js
+++ matthewrenda/client/src/Components/Footer.js
@@ -7,9 +7,7 @@ class Footer extends Component {
             <>
                 <footer>
                     <div className='footer-bgColor center blue'>
-                        <img className='footer-center' src={require('../imgs/portfolio_logo_yellow.png')}
-                            width='50px'
-                            height='50px' 
+                        <img className='footer-center footer-img' src={require('../imgs/portfolio_logo_yellow.png')}
                             alt="Matthew Renda logo"/>
                         <div className='footer-txtCenter'> Copyright &copy; 2020, MatthewJRenda</div>
 
